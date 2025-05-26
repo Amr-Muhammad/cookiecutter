80f46817be22211562024568c541043c73346bad
Mohammed Y. Alnajdi <malnajdi@42.solutions>
2025-05-26 14:06:58 +0300

    chore: remove folder committed by mistake

--- 1/cases/home.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { getFile } from "@tests/core/config";
-
-// test.use({ storageState: getFile("../.auth/applicant.json") });
-// test("", async ({ page }) => {});
